f09c56ac6007a1376e22cdedd3933e50bba967be
saint-sidhu <rreed8249@gmail.com>
2024-03-26 23:12:40 +0530

    Commit 3 # Experimenting with queues in activeMQ(JMS)

--- camel-microservice-b/src/main/java/dev/overlord/camelmicroserviceb/rasteyy/ActiveMQReceiverFromFile.java
+++ camel-microservice-b/src/main/java/dev/overlord/camelmicroserviceb/rasteyy/ActiveMQReceiverFromFile.java
@@ -3,17 +3,32 @@ package dev.overlord.camelmicroserviceb.rasteyy;
 import dev.overlord.camelmicroserviceb.CurrencyExchange;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.model.dataformat.JsonLibrary;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
 @Component
 public class ActiveMQReceiverFromFile extends RouteBuilder {
+
+
+
+    @Autowired
+    private MyCurrencyExchangeProcessor myCurrencyExchangeProcessor;
     @Override
     public void configure() throws Exception {
         from("activemq:activemq-file-queue")
                 .unmarshal().json(JsonLibrary.Jackson, CurrencyExchange.class)
+                .bean(myCurrencyExchangeProcessor)
                 .to("log:received-message-from-file-queue");
     }
 }
+class MyCurrencyExchangeProcessor{
+    private final Logger logger = LoggerFactory.getLogger(ActiveMQReceiverFromFile.class);
+    public void processMessage(CurrencyExchange currencyExchange){
 
+        logger.info("Do some processing with currencyExchnage",currencyExchange.getConversionMultiple());
+    }
+}
 
